81dcdd9f71f096f35716e82de705a43edad32e9f
Aditya <6984akv@gmail.com>
2023-08-24 16:49:06 +0530

    about page

--- pages/about.js
+++ pages/about.js
@@ -1,22 +1,75 @@
 // pages/about.js
-import React from 'react';
-import Header from '../components/Header';
+import React from "react";
+import Header from "../components/Header";
 
 const About = () => {
   return (
     <div>
-      
-      <section className="bg-gray-100 p-6">
+      <section className="bg-white p-6">
         <div className="container mx-auto">
           <h2 className="text-2xl font-semibold mb-4">Our Story</h2>
-          <p className="text-gray-700">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget aliquet est, eu efficitur nisi.
-            {/* Add your story here */}
+          <p className="text-gray-700 flex capitalize justify-center items-center  font-oswald">
+            IN the heart of the urban jungle, three passionate souls converged
+            over a shared grievance: the glaring gap in the footwear market.
+            Aditiya, a tech-savvy visionary; Nevil, a market analyst with an
+            astute sense of fashion trends; and Mayank, a design enthusiast with
+            an eye for detail, collectively observed that genuine, comfortable,
+            and stylish shoes were challenging to come by, particularly when one
+            sought specific sizes and hues.
+            <br />
+            <br />
+            Founded in 2023, SWAP-SHOES emerged not just as an e-commerce
+            platform but as a revolution. A revolution that aspired to marry
+            comfort with style, ensuring no foot was left shoe-less due to size
+            constraints or palette preferences.
+            <br />
+            <br />
+            Our founder's journey began on a summer evening, where over cups of
+            coffee and personal shoe-hunting tales, they sketched the dream of
+            SWAP_SHOES. Aditiya, always the problem solver, had frequently
+            grappled with the lack of technical sophistication in many online
+            shoe stores. Nevil, on the other hand, often pointed out the
+            mismatch between market demand and available shoe designs. Mayank,
+            the creative spirit, always lamented the mundane shades and
+            lackluster designs that dominated the shoe world. These shared
+            experiences became the foundation stones of SWAP_SHOES.
+            <br />
+            <br />
+            We, atSWAP_SHOES, believe that shoes are not just an accessory but
+            an extension of one’s personality. They can be bold declarations of
+            one’s ethos or subtle nods to understated elegance. With this
+            philosophy,SWAP_SHOES curates collections that cater to diverse
+            tastes, ensuring that each pair speaks volumes about the one who
+            wears them.
+            <br />
+            <br />
+            Our commitment extends beyond merely selling shoes. We’ve poured
+            countless hours into understanding the anatomy of the foot, ensuring
+            our shoes offer unparalleled comfort. With an expansive size range
+            and a kaleidoscope of colors,SWAP_SHOES stands as a testament to
+            inclusivity, guaranteeing there’s something for everyone.
+            <br />
+            <br />
+
+            As a company founded by three individuals who’ve walked (sometimes
+            uncomfortably, due to unsuitable shoes) in diverse paths of life, we
+            promise to walk alongside you, making every step you take confident
+            and comfortable.
+            <br />
+            <br />
+
+            Join us on this journey. Let’s tread new paths, scale new heights,
+            and walk towards brighter horizons, all while wearing the perfect
+            pair fromSWAP_SHOES.
+            <br />
+            <br />
+            Warmly,
+            <br />
+            <br />
+            Aditiya, Nevil, & Mayank
           </p>
         </div>
       </section>
-
-
     </div>
   );
 };
