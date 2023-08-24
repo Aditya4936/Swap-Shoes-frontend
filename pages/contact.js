7eecf653aa55ebee61f062d93b7cd564f4ec5d03
Aditya <6984akv@gmail.com>
2023-08-24 14:40:04 +0530

    contactpage

--- pages/contact.js
+++ pages/contact.js
@@ -1,86 +1,73 @@
-import React from 'react';
+import React from "react";
 
-const Contact = () => {
+const contact = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <div className="max-w-md w-full bg-white rounded-lg shadow-lg p-8">
+    <div name="Contact" className="w-full h-full bg-white">
+      <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
+        
         <div className="mb-8 text-center">
           <img
             className="mx-auto h-12 w-auto"
             src="/logo.svg"
             alt="Nike Logo"
           />
-          <h2 className="text-2xl font-semibold text-gray-800">
+          <h2 className="text-2xl font-semibold  text-gray-800">
             Contact Swap-Shoes
           </h2>
         </div>
-    
-        <form action='https://getform.io/f/f769210d-de7a-4cb4-a3c0-45c804e749fc' method='POST'>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-semibold mb-2"
-              htmlFor="name"
-            >
-              Name
-            </label>
+        <div className="flex justify-center items-center ">
+          <form
+            action="https://getform.io/f/f769210d-de7a-4cb4-a3c0-45c804e749fc"
+            method="POST"
+            className="flex flex-col w-full md:w-1/2 rounded-md"
+          >
             <input
-              className="w-full px-3 py-2 border  rounded-3xl focus:outline-none focus:ring focus:border-blue-500"
-              id="name"
               type="text"
-              placeholder="Enter your name"
+              name="name"
+              required
+              placeholder="Enter your Name"
+              className="p-2
+                bg-transparent border-2  rounded-3xl text-black focus:outline-none"
             />
-          </div>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-semibold mb-2"
-              htmlFor="email"
-            >
-              Email
-            </label>
             <input
-              className="w-full px-3 py-2 border  rounded-3xl focus:outline-none focus:ring focus:border-blue-500"
-              id="email"
-              type="email"
-              placeholder="Enter your email"
+              type="text"
+              name="email"
+              placeholder="Enter your Email"
+              pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+[a-z]{2,}$"
+              required
+              className="p-2
+               my-4 bg-transparent border-2 rounded-3xl text-black focus:outline-none"
             />
-          </div>
-          <div className="mb-6">
-            <label
-              className="block text-gray-700 text-sm font-semibold mb-2"
-              htmlFor="message"
-            >
-              Message
-            </label>
             <textarea
-              className="w-full h-24 px-3 py-2 border rounded-3xl focus:outline-none focus:ring focus:border-blue-500"
-              id="message"
-              placeholder="Enter your message"
-            />
-          </div>
-          <div className="flex items-center justify-center">
+              name="message"
+              rows="5"
+              placeholder="Enter Message"
+              required
+              className="p-2
+                bg-transparent border-2 rounded-3xl text-black focus:outline-none"
+            ></textarea>
+            <div className="mb-4 mt-5">
+              <p className="text-gray-700 text-sm">
+                For inquiries, you can reach us at:
+              </p>
+              <p className="text-blue-500 font-semibold">
+                Email: contact@Swap-Shoes.com
+              </p>
+              <p className="text-blue-500 font-semibold">
+                Phone: 1-800-Swap-Shoes-INFO
+              </p>
+            </div>
             <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg focus:outline-none focus:ring focus:border-blue-500"
-              type="button"
+              className="text-white bg-gradient-to-b from-black to-gray-600 
+                px-6 py-3 my-8 mx-auto flex item-center rounded-md hover:scale-110 duration-300"
             >
-              Send Message
+              Let's talk
             </button>
-            
-          </div>
-          <div className="mb-4">
-          <p className="text-gray-700 text-sm">
-            For inquiries, you can reach us at:
-          </p>
-          <p className="text-blue-500 font-semibold">
-            Email: contact@Swap-Shoes.com
-          </p>
-          <p className="text-blue-500 font-semibold">
-            Phone: 1-800-Swap-Shoes-INFO
-          </p>
+          </form>
         </div>
-        </form>
       </div>
     </div>
   );
 };
 
-export default Contact;
+export default contact;
